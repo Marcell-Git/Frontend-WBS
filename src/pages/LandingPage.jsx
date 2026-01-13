604289811b9138a9b1da52e3160d5bad2f693fd0
Marcell-Git <220711662@students.uajy.ac.id>
2026-01-13 14:01:13 +0700

    penyesuaian

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,29 +1,5 @@
 import Navbar from '../components/navbar';
 import Footer from '../components/Footer';
-import LandingImage1 from '../assets/LandingImage1.svg';
-import LandingImage2 from '../assets/LandingImage2.svg';
-import { LuListChecks } from 'react-icons/lu';
-import {
-  FaCircle,
-  FaClock,
-  FaLock,
-  FaMapMarkedAlt,
-  FaQuestionCircle,
-  FaSave,
-  FaScroll,
-  FaSearch,
-  FaUser,
-} from 'react-icons/fa';
-import { GoAlertFill } from 'react-icons/go';
-import {
-  FaShield,
-  FaFile,
-  FaFileCircleCheck,
-  FaUpload,
-  FaPhone,
-  FaLocationDot,
-} from 'react-icons/fa6';
-import { MdEmail } from 'react-icons/md';
 import Hero from '../components/Landing/Hero';
 import Tentang from '../components/Landing/Tentang';
 import Prosedur from '../components/Landing/Prosedur';
